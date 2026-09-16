ab3be940f486d69e9f62d592825c3004771b78db
Raquel Palis <raquelpalis@Raquels-MacBook-Pro.local>
2026-09-16 18:07:59 +0200

    Fix navigation export collision

--- src/components/index.ts
+++ src/components/index.ts
@@ -15,4 +15,5 @@ export * from './Card';
 export * from './ActionControls';
 export * from './FormLogIn';
 export * from './InputControls';
+export { NavigationButton, NavigationPillList, Tabs } from './NavigationAdvanced';
 export * from './TagToggle';
